25033dde9077eedbf7acbde3938ed663e51c244e
Dany Sluijk <me@dany.dev>
2022-07-26 22:04:11 +0200

    feat: add button example

---
+++ components/LinkButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+    width: 10rem;
+    height: 3rem;
+    border-radius: 10px;
+    background: #000;
+    color: #fff;
+    padding: 1rem;
+    margin: 1rem auto;
+
+    &:hover {
+        cursor: pointer;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
     <div>This is a page.</div>
+    <LinkButton @click="showBoop = !showBoop">Click me!</LinkButton>
+    <div v-if="showBoop">Boop! Thank you.</div>
 </template>
+
+<script lang="ts">
+export default {
+    data: function () {
+        return {
+            showBoop: false,
+        };
+    },
+};
+</script>
