2ef6b3e01463d3f405cd17285ff4159835100b55
Ardy <ardy@elnino.tech>
2022-07-30 22:27:39 +0200

    First version of the intro image

---
+++ components/IntroImage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="introImage">
+        <img class="introImage__title" src="/Images/Area51Lan Yellow.png" />
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,18 @@
 <template>
+    <IntroImage></IntroImage>
     <div>This is a page.</div>
     <LinkButton @click="showBoop = !showBoop">Click me!</LinkButton>
     <div v-if="showBoop">Boop! Thank you.</div>
 </template>
 
 <script lang="ts">
+import IntroImage from "~~/components/IntroImage.vue";
 export default {
     data: function () {
         return {
             showBoop: false,
         };
     },
+    components: { IntroImage }
 };
 </script>
