dfab01097dca534f32797d5c0240f2670f37566b
Ardy <a.zwanenburg@student.tudelft.nl>
2022-10-18 19:28:06 +0200

    First try making the login page design :)

---
+++ components/TextIamgeTransparent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="textimagetransparent">
+        <img class="image" :src="image"/>
+        <div class="content">
+            <h1>Login</h1>
+            <input 
+                type="text"
+                placeholder="Username"
+            >
+            <input 
+                type="password"
+                placeholder="Password"
+            >
+        </div>
+    </div>
+</template>
+    
+<style lang="scss" scoped>
+    .image {
+        width: 100%;
+        height: 100vh;
+        object-fit: cover;
+    }
+    
+    .content {
+        position: absolute;
+        right: 0;
+        top: 0;
+        width: 40vw;
+        height: 100vh;
+        background-color: rgba($color: #ffff, $alpha: .5);
+        padding-left: 5%;
+        padding-top: 10%;
+    }
+    
+    .content h1{
+        font-size: var(--font-size-xl);
+    }
+    
+    .content input{
+        width: 50%;
+        margin: .5rem 0rem;
+    }
+    
+    .textimagetransparent {
+        position: relative;
+        text-align: left;
+        color: white;
+    }
+    
+</style>
+<script>
+    export default {
+      props: {
+        image: String
+      }
+    }
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login">
+        <TextIamgeTransparent image="/images/gamers.jpg"></TextIamgeTransparent>
+    </div>
+</template>
+
+<script>
+import TextImageTransparent from "../components/TextIamgeTransparent.vue"
+    export default {
+      components: {
+        TextImageTransparent,
+      }
+    }
+</script>
