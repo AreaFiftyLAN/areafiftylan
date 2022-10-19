5b4c8ff26bb49b64acff6942a37523cd257b9dea
ajaggoe <a.s.h.jaggoe@student.tudelft.nl>
2022-10-19 11:54:56 +0200

    made automatic image scroller with css, now hardcoded animation

--- components/ImageScroll.vue
+++ components/ImageScroll.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
-        <h2 class="sponsor_category_title">{{ title }}</h2>
-        <div v-for="i in images" :key="i" class="container">
-            <div class="images_container">
-                <img :src="i" />
+        <h2 class="image_scroller_title">{{ title }}</h2>
+        <div class="container">
+            <div v-for="i in images" :key="i" class="image_container">
+                <NuxtImg :src="i" />
             </div>
+            <div class="image_container">
+                <NuxtImg :src="images[0]" />
+            </div>
+            <div class="image_container">
+                <NuxtImg :src="images[1]" />
+            </div>
+            <input id="count" type="number" :value="images.length" />
         </div>
     </div>
 </template>
@@ -19,21 +26,54 @@ defineProps<Props>();
 </script>
 
 <style lang="scss" scoped>
-.container {
-    display: flex;
-    justify-content: space-between;
+:root {
+    --end-translate: 0;
 }
 
-.inverse {
-    flex-direction: row-reverse;
+@keyframes scroll {
+    0% {
+        transform: translateX(0);
+    }
+    100% {
+        transform: translateX(-300%);
+    }
+}
+
+.container {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    overflow-x: auto;
+    counter-reset: c 0;
+    height: 80%;
+    z-index: 2;
+}
+.container .image_container {
+    animation: scroll 35s linear infinite;
+    counter-increment: c 1;
+}
+.container:hover .image_container {
+    animation-play-state: paused;
+}
+
+::-webkit-scrollbar {
+    height: 0;
+}
+
+.image_scroller_title {
+    display: flex;
+    position: absolute;
+    padding-top: var(--indent-m);
+    margin-left: 50%;
+    z-index: 1;
+}
+
+.image_container img {
+    -webkit-transition: all 0.5s cubic-bezier(0.19, 1, 0.22, 1);
 }
 .content {
     background: var(--theme-color);
     flex-grow: 1;
     padding: var(--indent-xl);
-}
-
-img {
-    max-width: 66%;
 }
 </style>
