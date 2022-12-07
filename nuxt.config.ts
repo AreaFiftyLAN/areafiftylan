80bd4ea2f719453d9c9b1ce89bdd29099f12f169
Dany Sluijk <me@dany.dev>
2022-12-07 20:39:17 +0100

    feat: update configuration files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,22 @@ export default defineNuxtConfig({
     typescript: {
         strict: true,
     },
-    image: {},
-    vite: {
-        server: {
-            hmr: {
-                protocol: "ws",
-            },
+    runtimeConfig: {
+        seed: false,
+    },
+    app: {
+        head: {
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1",
+            title: "AreaFiftyLAN",
+            meta: [
+                {
+                    name: "description",
+                    content: "AreaFiftyLAN - Gaming Event",
+                },
+                { name: "theme-color", content: "#1a2b43" },
+            ],
+            link: [{ rel: "icon", href: "/invader.png" }],
         },
     },
 });
